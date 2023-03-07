e723f2efa9828a9d303690a2e533d2bde6dd0d46
Frederik Espersen Knudsen <frederikespersen@icloud.com>
2023-03-07 18:47:42 +0100

    Update analyse_utils.py

--- src/analyse_utils.py
+++ src/analyse_utils.py
@@ -195,6 +195,54 @@ def amino_acid_content(seqs: str|list|pd.Series) -> pd.DataFrame:
 
 
 #························································································#
+def average_sequence(seqs: list|pd.Series) -> str:
+    """
+    
+    Takes a list or Series of sequences, returns an average sequence.
+    
+    Averaging occurs by first generating the average amino acid counts and then 
+    assembling a random sequence from that.
+    The average amino acid counts are found by multiplying the average amino acid
+    frequency of each amino acid by the average sequence length.
+
+    --------------------------------------------------------------------------------
+
+    Parameters
+    ----------
+
+        `seqs`: `list|pandas.Series`
+            Sequences to average
+
+    Returns
+    -------
+
+        `avg`: `str`
+            The generated average series
+    
+    """
+
+    # Setting datatype to Series
+    seqs = pd.Series(seqs)
+
+    # Finding average amino acid frequencies
+    freqs = amino_acid_content(seqs).mean()
+
+    # Finding average length
+    N = seqs.str.len().mean()
+
+    # Finding average amino acid counts
+    counts = freqs * N
+
+    # Assembling random sequence using counts
+    avg = ''
+    for aa, c in counts.items():
+        avg += aa * round(c)
+    avg = data_utils.shuffle_seq(avg, seed=1)
+
+    return avg
+
+
+#························································································#
 #··································· C I D E R ··········································#
 #························································································#
 
@@ -245,54 +293,6 @@ def cider_parameters(seqs: str|list|pd.Series) -> pd.DataFrame:
     params['Frac. dis. prom.'] = seqs.apply(lambda SeqOb: SeqOb.get_fraction_disorder_promoting())
 
     return params
-
-
-#························································································#
-def average_sequence(seqs: list|pd.Series) -> str:
-    """
-    
-    Takes a list or Series of sequences, returns an average sequence.
-    
-    Averaging occurs by first generating the average amino acid counts and then 
-    assembling a random sequence from that.
-    The average amino acid counts are found by multiplying the average amino acid
-    frequency of each amino acid by the average sequence length.
-
-    --------------------------------------------------------------------------------
-
-    Parameters
-    ----------
-
-        `seqs`: `list|pandas.Series`
-            Sequences to average
-
-    Returns
-    -------
-
-        `avg`: `str`
-            The generated average series
-    
-    """
-
-    # Setting datatype to Series
-    seqs = pd.Series(seqs)
-
-    # Finding average amino acid frequencies
-    freqs = amino_acid_content(seqs).mean()
-
-    # Finding average length
-    N = seqs.str.len().mean()
-
-    # Finding average amino acid counts
-    counts = freqs * N
-
-    # Assembling random sequence using counts
-    avg = ''
-    for aa, c in counts.items():
-        avg += aa * round(c)
-    avg = data_utils.shuffle_seq(avg, seed=1)
-
-    return avg
 
 
 #························································································#
