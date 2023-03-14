8ff170619a32abef95ddaf117c86d9326542a98b
frederikespersen <frederikespersen@icloud.com>
2023-03-14 17:07:48 +0100

    Added submit template

--- src/README.md
+++ src/README.md
@@ -8,11 +8,11 @@ This directory contains:
 - `utils.py`: Script with general utils
 - `data_utils.py`: Script for generating input data
 - `analyse_utils.py`: Script for analysis
-- `simulate.py`: Script for submitting a simulation
-- `simulate_utils.py`: Utilitary functions for `simulate.py`
+- `simulate_utils.py`: Script for simulations
+- `simulate_openmm.py`: Submission script for submitting an OpenMM simulation
 - `evolve.py`: Script for submitting an evolution
 - `evolve_utils.py`: Utilitary functions for `evolve.py`
 - `residues.py`: Residue-level data and parameters as a Python object
 - `conditions.py`: Standard condition sets foorsimulatin as a Python object
 - **`templates/`**: Templates to fill out for submitting simulations
-    - `run_simulate.sh`: **Template** Shell script for submitting a simulation with Slurm
+    - `submit.sh`: **Template** Shell script for submitting a simulation with Slurm
---
+++ src/templates/submit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+###################################################################################
+# SUBMIT Template
+# ---------------
+# Template for a simulation submission script; Fill out fields marked by '<field>'.
+# Place the file in the corresponding ~/exp/< >/ folder.
+###################################################################################
+
+#SBATCH --job-name=<jobname>
+#SBATCH --partition=<partition>
+#SBATCH --array=<range>%<simultaneous>
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH -t <walltime>
+#SBATCH -o results/out
+#SBATCH -e results/err
+
+
+# Getting job input file
+input_files=($(ls data/*.fasta))
+input_file=${input_files[$SLURM_ARRAY_TASK_ID]}
+
+# Displaying job info
+echo "[`date`] STARTED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
+
+# DeiC env settings
+source <source>
+conda <env>
+
+# Submitting simulation
+python ../../src/simulate_openmm.py -f $input_file <options>
+
+echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
