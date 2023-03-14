c3c0fa10f887c04b187ed3d61ea91c366705aba0
frederikespersen <frederikespersen@icloud.com>
2023-03-14 12:21:57 +0100

    Ortho submit

--- exp/ortho_h1-0/submit.sh
+++ exp/ortho_h1-0/submit.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=ortho_h1-0
-#SBATCH --ntasks=1
-#SBATCH --partition=sbinlab_gpu
+#SBATCH --partition=qgpu
 #SBATCH --array=0-190%10
-#SBATCH --nodes=1
+#SBATCH --nodes=10
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH -t 24:00:00
 #SBATCH -o results/out
 #SBATCH -e results/err
@@ -18,8 +17,8 @@ input_file=${input_files[$SLURM_ARRAY_TASK_ID]}
 # Displaying job info
 echo "[`date`] STARTED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
 
-# DeiC env settings
-source /groups/sbinlab/fpesce/.bashrc
+# Robust env settings
+source /home/fpesce/.bashrc
 conda activate openmm
 
 # Submitting simulation
