1d47797309914e04b622ca38ccd7521a0fff06f6
Frederik Espersen Knudsen <fknudsen@fend01.cluster>
2023-03-17 14:32:19 +0100

    Test nsteps fix

--- exp/ortho_h1-0/submit_0-9_test.sh
+++ exp/ortho_h1-0/submit_0-9_test.sh
@@ -21,6 +21,6 @@ source /groups/sbinlab/fpesce/.bashrc
 conda activate openmm
 
 # Submitting simulation
-python ../../src/simulate_openmm.py -f $input_file -p CPU -s 100000
+python ../../src/simulate_openmm.py -f $input_file -p CPU -n 100000
 
 echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
