f813fd4fdfe4fe8ba89b2fdc4d7946b8470836e0
Frederik Espersen Knudsen <fknudsen@fend01.cluster>
2023-05-02 18:14:52 +0200

    Run on CPU

--- exp/idpevo_h1-0/submit_evo_cons.sh
+++ exp/idpevo_h1-0/submit_evo_cons.sh
@@ -3,9 +3,7 @@
 #SBATCH --partition=sbinlab_ib2
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
-#SBATCH -t 168:00:00            # 1 week
-#SBATCH --mem=16G
+#SBATCH -t 12:00:00            # 1/2 day
 #SBATCH -o results/cons.out
 #SBATCH -e results/cons.err
 
@@ -20,6 +18,6 @@ source /groups/sbinlab/fpesce/.bashrc
 conda activate openmm
 
 # Submitting simulation
-python ../../src/evolve.py --dir cons --fasta $input_file --measure kappa --target 0.15 --L_at_half_acceptance 0.001 --simulated_annealing
+python ../../src/evolve.py --restart -1 --dir cons --fasta $input_file --measure kappa --target 0.1889910274293869 --L_at_half_acceptance 0.01 --simulated_annealing
 
 echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
--- exp/idpevo_h1-0/submit_evo_min.sh
+++ exp/idpevo_h1-0/submit_evo_min.sh
@@ -3,9 +3,7 @@
 #SBATCH --partition=sbinlab_ib2
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
-#SBATCH -t 168:00:00            # 1 week
-#SBATCH --mem=16G
+#SBATCH -t 12:00:00            # 1/2 day
 #SBATCH -o results/min.out
 #SBATCH -e results/min.err
 
@@ -20,6 +18,6 @@ source /groups/sbinlab/fpesce/.bashrc
 conda activate openmm
 
 # Submitting simulation
-python ../../src/evolve.py --dir min --fasta $input_file --measure kappa --target 0 --L_at_half_acceptance 0.001 --simulated_annealing
+python ../../src/evolve.py --restart -1 --dir min --fasta $input_file --measure kappa --target 0 --L_at_half_acceptance 0.01 --simulated_annealing
 
 echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file"
