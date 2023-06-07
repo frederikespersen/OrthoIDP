10f79acb9505778b299fd93ab65efdc4f5d5ac30
Frederik Espersen Knudsen <fknudsen@fend01.cluster>
2023-06-07 21:24:58 +0200

    Submit changes

--- exp/prota_variant_v3/submit_single_chain.sh
+++ exp/prota_variant_v3/submit_single_chain.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=sc_prota_variant
-#SBATCH --partition=sbinlab_gpu
-#SBATCH --array=0-19%4
+#SBATCH --partition=sbinlab_ib2
+#SBATCH --array=0-19%20
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
 #SBATCH -t 168:00:00                 # 1 week
 #SBATCH -o results/single_chain.out
 #SBATCH -e results/single_chain.err
 
 
 # Setting input options
-input_seqs=(H1-0_VAR_k0.60 H1-0_VAR_k0.21 H1-0_VAR_k0.85 H1-0_VAR_k0.40 H1-0_VAR_k0.07 H1-0_VAR_k0.70 H1-0_VAR_k0.50 H1-0_VAR_k0.30 H1-0_VAR_k0.92 H1-0_VAR_k0.75 H1-0_VAR_k0.55 H1-0_VAR_k0.14 H1-0_VAR_k0.65 H1-0_VAR_k0.80 H1-0_VAR_k0.46 H1-0_VAR_k0.89 H1-0_VAR_k0.36 H1-0_VAR_k0.18 H1-0_VAR_k0.27 H1-0_VAR_k0.11)
+input_seqs=(H1-0_VAR_Rg3.44  H1-0_VAR_Rg3.61  H1-0_VAR_Rg3.75  H1-0_VAR_Rg3.86  H1-0_VAR_Rg3.96  H1-0_VAR_Rg4.07  H1-0_VAR_Rg4.22 H1-0_VAR_Rg3.50  H1-0_VAR_Rg3.66  H1-0_VAR_Rg3.79  H1-0_VAR_Rg3.90  H1-0_VAR_Rg3.99  H1-0_VAR_Rg4.15  H1-0_VAR_Rg4.25 H1-0_VAR_Rg3.56  H1-0_VAR_Rg3.72  H1-0_VAR_Rg3.83  H1-0_VAR_Rg3.93  H1-0_VAR_Rg4.03  H1-0_VAR_Rg4.19)
 input_cond="default"
 
 # Get the current sequence
@@ -27,6 +25,6 @@ source /groups/sbinlab/fpesce/.bashrc
 conda activate openmm
 
 # Submitting simulation
-python ../../src/simulate_openmm_fasta.py -f $input_file -c $input_cond -d $output_dir -n 10000000 -b 100
+python ../../src/simulate_openmm_fasta.py -f $input_file -c $input_cond -d $output_dir -n 10000000 -b 100 -p CPU
 
 echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file; $input_cond"
