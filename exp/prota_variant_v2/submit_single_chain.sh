8326b71ab2e1309f8a9afd1fc6bae18efb1ebdc1
Frederik Espersen Knudsen <fknudsen@fend01.cluster>
2023-06-07 18:52:18 +0200

    Submission changes

--- exp/prota_variant_v2/submit_single_chain.sh
+++ exp/prota_variant_v2/submit_single_chain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=sc_prota_variant
 #SBATCH --partition=sbinlab_gpu
-#SBATCH --array=0-19%4
+#SBATCH --array=0-19%20
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
