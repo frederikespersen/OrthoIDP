4705d1759a84faa615cf6cdcbe3f26287edbce82
Frederik Espersen Knudsen <fknudsen@s96n41.grendel.cscaa.dk>
2023-06-08 13:21:42 +0200

    Submisssion changes

--- exp/prota_variant_k/submit_energy_calc.sh
+++ exp/prota_variant_k/submit_energy_calc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=ec_prota_variant
-#SBATCH --partition=sbinlab_ib
+#SBATCH --job-name=ec_k_prota
+#SBATCH --partition=qgpu
 #SBATCH --array=0-19%20
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
@@ -11,7 +11,7 @@
 
 
 # Setting input options
-input_tops=(H1-0_VAR_k0.60_PROTA_WT_25nm H1-0_VAR_k0.21_PROTA_WT_25nm H1-0_VAR_k0.85_PROTA_WT_25nm H1-0_VAR_k0.40_PROTA_WT_25nm H1-0_VAR_k0.07_PROTA_WT_25nm H1-0_VAR_k0.70_PROTA_WT_25nm H1-0_VAR_k0.50_PROTA_WT_25nm H1-0_VAR_k0.30_PROTA_WT_25nm H1-0_VAR_k0.92_PROTA_WT_25nm H1-0_VAR_k0.75_PROTA_WT_25nm H1-0_VAR_k0.55_PROTA_WT_25nm H1-0_VAR_k0.14_PROTA_WT_25nm H1-0_VAR_k0.65_PROTA_WT_25nm H1-0_VAR_k0.80_PROTA_WT_25nm H1-0_VAR_k0.46_PROTA_WT_25nm H1-0_VAR_k0.89_PROTA_WT_25nm H1-0_VAR_k0.36_PROTA_WT_25nm H1-0_VAR_k0.18_PROTA_WT_25nm H1-0_VAR_k0.27_PROTA_WT_25nm H1-0_VAR_k0.11_PROTA_WT_25nm)
+input_tops=(H1-0_VAR_k0.07_PROTA_WT_25nm H1-0_VAR_k0.20_PROTA_WT_25nm H1-0_VAR_k0.36_PROTA_WT_25nm H1-0_VAR_k0.50_PROTA_WT_25nm H1-0_VAR_k0.65_PROTA_WT_25nm H1-0_VAR_k0.79_PROTA_WT_25nm H1-0_VAR_k0.93_PROTA_WT_25nm H1-0_VAR_k0.11_PROTA_WT_25nm H1-0_VAR_k0.27_PROTA_WT_25nm H1-0_VAR_k0.40_PROTA_WT_25nm H1-0_VAR_k0.55_PROTA_WT_25nm H1-0_VAR_k0.70_PROTA_WT_25nm H1-0_VAR_k0.84_PROTA_WT_25nm H1-0_VAR_k0.98_PROTA_WT_25nm H1-0_VAR_k0.14_PROTA_WT_25nm H1-0_VAR_k0.30_PROTA_WT_25nm H1-0_VAR_k0.46_PROTA_WT_25nm H1-0_VAR_k0.60_PROTA_WT_25nm H1-0_VAR_k0.75_PROTA_WT_25nm H1-0_VAR_k0.90_PROTA_WT_25nm)
 input_cond="ionic_240"
 
 # Getting arguments
@@ -24,9 +24,9 @@ output_file="$input_dir/interaction_energy.csv"
 # Displaying job info
 echo "[`date`] STARTED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_traj; $input_top"
 
-# DeiC env settings
-source /groups/sbinlab/fpesce/.bashrc
-conda activate openmm
+# ROBUST env settings
+source /home/fknudsen/.bashrc
+conda activate orthoidp
 
 # Submitting simulation
 python ../../src/interaction_energy.py -t $input_traj -p $input_top -x "chainid 0" -y "chainid 1" -c $input_cond -o $output_file --com --minimum_inter
--- exp/prota_variant_k/submit_two_chain.sh
+++ exp/prota_variant_k/submit_two_chain.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=qgpu
 #SBATCH --array=0-19%20
 #SBATCH --gres=gpu:1
-#SBATCH -t 48:00:00                # 2 days
+#SBATCH -t 72:00:00                 # 3 days
 #SBATCH -o results/two_chain.out
 #SBATCH -e results/two_chain.err
 
 
 # Setting input options
-input_tops=(H1-0_VAR_k0.07_PROTA_WT_25nm  H1-0_VAR_k0.27_PROTA_WT_25nm  H1-0_VAR_k0.40_PROTA_WT_25nm  H1-0_VAR_k0.55_PROTA_WT_25nm  H1-0_VAR_k0.70_PROTA_WT_25nm H1-0_VAR_k0.11_PROTA_WT_25nm  H1-0_VAR_k0.30_PROTA_WT_25nm  H1-0_VAR_k0.46_PROTA_WT_25nm  H1-0_VAR_k0.60_PROTA_WT_25nm  H1-0_VAR_k0.75_PROTA_WT_25nm H1-0_VAR_k0.14_PROTA_WT_25nm  H1-0_VAR_k0.36_PROTA_WT_25nm  H1-0_VAR_k0.50_PROTA_WT_25nm  H1-0_VAR_k0.65_PROTA_WT_25nm)
+input_tops=(H1-0_VAR_k0.07_PROTA_WT_25nm H1-0_VAR_k0.20_PROTA_WT_25nm H1-0_VAR_k0.36_PROTA_WT_25nm H1-0_VAR_k0.50_PROTA_WT_25nm H1-0_VAR_k0.65_PROTA_WT_25nm H1-0_VAR_k0.79_PROTA_WT_25nm H1-0_VAR_k0.93_PROTA_WT_25nm H1-0_VAR_k0.11_PROTA_WT_25nm H1-0_VAR_k0.27_PROTA_WT_25nm H1-0_VAR_k0.40_PROTA_WT_25nm H1-0_VAR_k0.55_PROTA_WT_25nm H1-0_VAR_k0.70_PROTA_WT_25nm H1-0_VAR_k0.84_PROTA_WT_25nm H1-0_VAR_k0.98_PROTA_WT_25nm H1-0_VAR_k0.14_PROTA_WT_25nm H1-0_VAR_k0.30_PROTA_WT_25nm H1-0_VAR_k0.46_PROTA_WT_25nm H1-0_VAR_k0.60_PROTA_WT_25nm H1-0_VAR_k0.75_PROTA_WT_25nm H1-0_VAR_k0.90_PROTA_WT_25nm)
 input_cond="ionic_240"
 
 # Get the current sequence
@@ -25,6 +25,6 @@ source /home/fknudsen/.bashrc
 conda activate orthoidp
 
 # Submitting simulation
-python ../../src/simulate_openmm_top.py -t $input_file -c $input_cond -d $output_dir -n 200000000
+python ../../src/simulate_openmm_top.py -t $input_file -c $input_cond -d $output_dir -n 1000000000
 
 echo "[`date`] FINISHED Job Array ID: $SLURM_ARRAY_TASK_ID | Job ID: $SLURM_JOB_ID | Input: $input_file; $input_cond"
