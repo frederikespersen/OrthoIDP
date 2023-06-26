062f43380f7606ef706ae4719c7cc5a7b478ce32
Frederik Espersen Knudsen <fknudsen@s96n41.grendel.cscaa.dk>
2023-06-26 14:47:43 +0200

    Submit sscripts

--- exp/prota_variant_rg/_submit_energy_calc.sh
+++ exp/prota_variant_rg/_submit_energy_calc.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=ec_rg_prota_variant
 #SBATCH --partition=qgpu
-#SBATCH --array=0-19%20
+#SBATCH --array=0-2%3
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 #SBATCH -t 24:00:00
-#SBATCH -o results/energy_calc.out
-#SBATCH -e results/energy_calc.err
+#SBATCH -o results/_energy_calc.out
+#SBATCH -e results/_energy_calc.err
 
 
 # Setting input options
-input_tops=(H1-0_VAR_Rg3.44_PROTA_WT_25nm H1-0_VAR_Rg3.61_PROTA_WT_25nm H1-0_VAR_Rg3.75_PROTA_WT_25nm H1-0_VAR_Rg3.86_PROTA_WT_25nm H1-0_VAR_Rg3.96_PROTA_WT_25nm H1-0_VAR_Rg4.07_PROTA_WT_25nm H1-0_VAR_Rg4.22_PROTA_WT_25nm H1-0_VAR_Rg3.50_PROTA_WT_25nm H1-0_VAR_Rg3.66_PROTA_WT_25nm H1-0_VAR_Rg3.79_PROTA_WT_25nm H1-0_VAR_Rg3.90_PROTA_WT_25nm H1-0_VAR_Rg3.99_PROTA_WT_25nm H1-0_VAR_Rg4.15_PROTA_WT_25nm H1-0_VAR_Rg4.25_PROTA_WT_25nm H1-0_VAR_Rg3.56_PROTA_WT_25nm H1-0_VAR_Rg3.72_PROTA_WT_25nm H1-0_VAR_Rg3.83_PROTA_WT_25nm H1-0_VAR_Rg3.93_PROTA_WT_25nm H1-0_VAR_Rg4.03_PROTA_WT_25nm H1-0_VAR_Rg4.19_PROTA_WT_25nm)
-input_cond="ionic_240"
+input_tops=(H1-0_VAR_Rg4.19_PROTA_WT_25nm H1-0_VAR_Rg4.22_PROTA_WT_25nm H1-0_VAR_Rg4.25_PROTA_WT_25nm)
+input_cond="ionic_290"
 
 # Getting arguments
 input_top=${input_tops[$SLURM_ARRAY_TASK_ID]}
